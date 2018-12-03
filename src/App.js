39e1197a69e52c0ba6d1828dcaab0eae18e8de93
Hayri Simsek <hs@Hayri-MacBook-Pro.local>
2018-12-03 16:43:01 +0300

    project modified for new build

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Hello World!
+            Hello World, This is Travis Integration!
           </p>
           <a
             className="App-link"
